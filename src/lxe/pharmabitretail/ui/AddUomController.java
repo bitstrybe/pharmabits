16130a0dabd93ddffff4083aaff623d9cdae9082
Jeffrey O Adams <frexjeff@gmail.com>
2020-12-21 15:33:37 +0000

    Merge origin/master
    Conflicts:
    	src/lxe/pharmabitretail/ui/AddItemsController.java

--- src/lxe/pharmabitretail/ui/AddUomController.java
+++ src/lxe/pharmabitretail/ui/AddUomController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lxe.pharmabitretail.ui;
 
 import com.jfoenix.controls.JFXButton;
@@ -132,6 +128,7 @@ public class AddUomController implements Initializable {
     @FXML
     private void saveAction(ActionEvent event) {
 
+        System.out.println("testing addUOM");
         save.setDisable(true);
         Task<Void> task = new Task<Void>() {
             @Override
